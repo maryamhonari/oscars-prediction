10ba2bae41fe80333b3a0242ea4b6ccea1fffab9
Omar Elazhary <omazhary@gmail.com>
2016-10-25 11:12:23 -0700

    Using newer dataset.

--- clustering.py
+++ clustering.py
@@ -5,7 +5,8 @@ from preprocessor import DataPreprocessor
 
 preprocessor = DataPreprocessor(['Nominated Best Picture',
                                 'Won Best Picture', 'Num of Awards'],
-                                ['genres', 'plot_keywords'], 'Final.csv')
+                                ['genres', 'plot_keywords'],
+                                'final_dataset_no_duplicates.csv')
 preprocessor.preprocess()
 print len(preprocessor.features)
 print len(preprocessor.features[0])
