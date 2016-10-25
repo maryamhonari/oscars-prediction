bc788a90864f73e9e5cfce1096fbe2360fb77205
Omar Elazhary <omazhary@gmail.com>
2016-10-25 10:52:21 -0700

    Refactored for OOP usability

--- classify.py
+++ classify.py
@@ -2,45 +2,79 @@
 
 import csv
 
-# Data preprocessing:
-features = []
-colmap = dict()
-class_labels = [
-    'Nominated Best Picture',
-    'Won Best Picture',
-    'Num of Awards'
-]
-labels = []
-class_label_index = []
-ignore = ['genres', 'plot_keywords']
 
-for heading in class_labels:
-    labels.append([])
+class DataPreprocessor:
+    """This class transforms your csv files into inputs suitable for use with
+    scikit-learn."""
 
+    # Class variables
+    features = []
+    colmap = dict()
+    class_labels = [
+        'Nominated Best Picture',
+        'Won Best Picture',
+        'Num of Awards'
+    ]
+    labels = []
+    class_label_index = []
+    ignore = ['genres', 'plot_keywords']
+    csv_filename = ""
 
-# Load data:
-counter = 0
-with open("Final.csv", "rb") as dataset:
-    entryreader = csv.reader(dataset, delimiter=',')
-    for row in entryreader:
-        # Everything after column 8181 is a duplicate. File still needs cleaning up.
-        row = row[:8182]
-        if counter == 0:
-            # The first row is just column headings. It gets special treatment.
-            for column in row:
-                colmap[column] = row.index(column)
-        else:
-            # Need to separate features from processed and class label fields.
-            data_row = []
-            skippable_attributes = list(ignore)
-            skippable_attributes.extend(class_labels)
-            skippable_attributes = [colmap[x] for x in skippable_attributes]
-            for class_label in class_labels:
-                labels[class_labels.index(class_label)].append(row[colmap[class_label]])
-            for index, value in enumerate(row):
-                if index >= len(row):
-                    break
-                if index not in skippable_attributes:
-                    data_row.append(value)
-            features.append(data_row)
-        counter += 1
+    def __init__(self, label_column_names_list, column_names_to_ignore_list,
+                 csv_filename):
+        """
+        Instantiates a new DataPreprocessor object.
+        Parameters:
+            label_column_names_list - A list of column names that indicate
+            class labels.
+            column_names_to_ignore_list - A list of column names to ignore.
+            csv_filename - The name of the source csv file.
+        """
+        self.csv_filename = csv_filename
+        self.class_labels = label_column_names_list
+        self.ignore = column_names_to_ignore_list
+        for heading in self.class_labels:
+            self.labels.append([])
+
+    def preprocess(self):
+        """
+        Reads and preprocesses the data from a csv file to produce a large
+        2d-arrary containing features, and a smaller 2d-array containing
+        multiple labels where each is aligned with the features.
+        """
+        # Load data:
+        counter = 0
+        with open(self.csv_filename, "rb") as dataset:
+            entryreader = csv.reader(dataset, delimiter=',')
+            for row in entryreader:
+                # Everything after column 8181 is a duplicate. File still needs cleaning up.
+                row = row[:8182]
+                if counter == 0:
+                    # The first row is column headings (special treatment).
+                    for column in row:
+                        self.colmap[column] = row.index(column)
+                else:
+                    # Need to separate features from ignored and class label
+                    # fields.
+                    data_row = []
+                    skippable_attributes = list(self.ignore)
+                    skippable_attributes.extend(self.class_labels)
+                    skippable_attributes = [self.colmap[x] for x in
+                                            skippable_attributes]
+                    for class_label in self.class_labels:
+                        label_index = self.class_labels.index(class_label)
+                        column_index = row[self.colmap[class_label]]
+                        self.labels[label_index].append(column_index)
+                    for index, value in enumerate(row):
+                        if index not in skippable_attributes:
+                            data_row.append(value)
+                    self.features.append(data_row)
+                counter += 1
+
+"""
+preprocessor = DataPreprocessor(['Nominated Best Picture',
+                                'Won Best Picture', 'Num of Awards'],
+                                ['genres', 'plot_keywords'], 'Final.csv')
+preprocessor.preprocess()
+print len(preprocessor.features[0])
+"""
