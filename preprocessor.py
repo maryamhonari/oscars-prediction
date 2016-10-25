3a07b09f1b2f38f9df3ffe0680710c70d32eee2a
Omar Elazhary <omazhary@gmail.com>
2016-10-25 10:59:13 -0700

    Removed commented code from file.

--- preprocessor.py
+++ preprocessor.py
@@ -70,11 +70,3 @@ class DataPreprocessor:
                             data_row.append(value)
                     self.features.append(data_row)
                 counter += 1
-
-"""
-preprocessor = DataPreprocessor(['Nominated Best Picture',
-                                'Won Best Picture', 'Num of Awards'],
-                                ['genres', 'plot_keywords'], 'Final.csv')
-preprocessor.preprocess()
-print len(preprocessor.features[0])
-"""
