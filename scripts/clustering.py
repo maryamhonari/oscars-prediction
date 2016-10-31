ba5ce589273eaa2a28b30636c1759644e4773910
Omar Elazhary <omazhary@gmail.com>
2016-10-31 16:22:08 -0700

    Started working on clustering.
    - Still work in progress.
    - Added metrics to evaluate the clustering process.

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 from preprocessor import DataPreprocessor
+from sklearn.cluster import KMeans
+from sklearn import metrics
 
 """
 Applies clustering algorithms to the movie dataset in order to discover
@@ -15,5 +17,20 @@ preprocessor = DataPreprocessor(['Nominated Best Picture',
                                 ['genres', 'plot_keywords'],
                                 'final_dataset_no_duplicates.csv')
 preprocessor.preprocess()
-print len(preprocessor.features)
-print len(preprocessor.features[0])
+preprocessor.numerify()
+
+km = KMeans(n_clusters=2, init='k-means++', max_iter=5000, n_init=1,
+        verbose=False)
+
+km.fit(preprocessor.features_numerical)
+
+print("Homogeneity: %0.3f" % metrics.homogeneity_score(preprocessor.labels[0],
+    km.labels_))
+print("Completeness: %0.3f" % metrics.completeness_score(preprocessor.labels[0]
+    , km.labels_))
+print("V-Measure: %0.3f" % metrics.v_measure_score(preprocessor.labels[0],
+    km.labels_))
+print("Adjusted Rand-Index: %0.3f"
+        % metrics.adjusted_rand_score(preprocessor.labels[0], km.labels_))
+print("Silhouette Coefficient: %0.3f"
+        % metrics.silhouette_score(preprocessor.labels[0], km.labels_))
