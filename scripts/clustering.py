fd6dcc20bf9f56d60625e2e761ef024a201059ab
Omar Elazhary <omazhary@gmail.com>
2016-11-02 16:23:42 -0700

    Working on clustering - WiP

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -17,20 +17,32 @@ preprocessor = DataPreprocessor(['Nominated Best Picture',
                                 ['genres', 'plot_keywords'],
                                 'final_dataset_no_duplicates.csv')
 preprocessor.preprocess()
+
+preprocessor.add_feature(preprocessor.labels[0])
+
 preprocessor.numerify()
 
+preprocessor.create_test_set(0.3, 0, True)
+
+"""
 km = KMeans(n_clusters=2, init='k-means++', max_iter=5000, n_init=1,
         verbose=False)
 
-km.fit(preprocessor.features_numerical)
+km.fit_predict(preprocessor.features_numerical)
 
-print("Homogeneity: %0.3f" % metrics.homogeneity_score(preprocessor.labels[0],
-    km.labels_))
-print("Completeness: %0.3f" % metrics.completeness_score(preprocessor.labels[0]
-    , km.labels_))
-print("V-Measure: %0.3f" % metrics.v_measure_score(preprocessor.labels[0],
-    km.labels_))
+print km.labels_
+print preprocessor.labels_numerical[0]
+
+print("Homogeneity: %0.3f" % metrics.homogeneity_score(
+    preprocessor.labels_numerical[0], km.labels_))
+print("Completeness: %0.3f" % metrics.completeness_score(
+    preprocessor.labels_numerical[0] , km.labels_))
+print("V-Measure: %0.3f" % metrics.v_measure_score(
+    preprocessor.labels_numerical[0], km.labels_))
 print("Adjusted Rand-Index: %0.3f"
-        % metrics.adjusted_rand_score(preprocessor.labels[0], km.labels_))
+        % metrics.adjusted_rand_score(preprocessor.labels_numerical[0],
+            km.labels_))
 print("Silhouette Coefficient: %0.3f"
-        % metrics.silhouette_score(preprocessor.labels[0], km.labels_))
+        % metrics.silhouette_score(preprocessor.labels_numerical[0],
+            km.labels_))
+"""
