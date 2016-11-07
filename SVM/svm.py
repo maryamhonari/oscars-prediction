e1512db6d98fadda54c666935b31631f16951189
amashreghi <ali.mashreghi87@gmail.com>
2016-11-07 10:27:12 -0800

    Simplified svm which uses cross-validation for measuring performance of the classifier/regressor

--- SVM/svm.py
+++ SVM/svm.py
@@ -4,6 +4,12 @@ import csv
 import numpy as np
 import time
 import math
+from sklearn.metrics import explained_variance_score
+from sklearn import preprocessing
+from sklearn import cross_validation
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import r2_score
+
 
 #converts a csv file to 2D array
 def csvToArray(filename):
@@ -13,6 +19,68 @@ def csvToArray(filename):
         for row in entryreader:
             ret.append(row)
     return ret
+
+def getTrainTestRowsAndCols(labelOfInterest, titleYearIdx):
+    """
+
+    :param labelOfInterest: #0 means nominee , 1 means winner (for best picture), 2 means number of awards
+    :return:
+    """
+    trainRows = []
+    testRows = []
+
+    for i in range(len(features)):
+        if labelOfInterest != 1:
+            if int(math.floor(features[i][titleYearIdx])) % 4 == 0:
+                testRows.append(i)
+            else:
+                trainRows.append(i)
+        else:
+            if labels[i][0] == 1:
+                if int(math.floor(features[i][titleYearIdx])) % 4 == 0:
+                    testRows.append(i)
+                else:
+                    trainRows.append(i)
+
+    # prints percentage of train and test
+    print len(trainRows) / len(features), len(testRows) / len(features)
+
+    # feature selection based on correlation values
+    correlation = csvToArray("feature_correlation_results.csv")
+
+    favoriteCols = []
+    favoriteColsNames = []
+
+    threshold = 0.1
+    for i in range(1, len(correlation)):
+        if labelOfInterest == 2 and ('Won' in correlation[i][0]):  # we should ignore this when predicting num of awards
+            threshold = 0.1
+            continue
+
+        if labelOfInterest == 1 and ('Won' in correlation[i][0]):
+            threshold = 0.1
+            continue
+
+        if labelOfInterest == 0 and (('Won' in correlation[i][0])):
+            threshold = 0.1
+            continue
+
+        if correlation[i][0] in featIdxMap:
+            if math.fabs(float(correlation[i][1 + labelOfInterest])) > threshold:
+                # print correlation[i][0]
+                favoriteCols.append(featIdxMap[correlation[i][0]])
+                favoriteColsNames.append(correlation[i][0])
+
+    print 'favoritCols = ', len(favoriteCols)
+
+    print favoriteColsNames
+
+    # making test set half positive and half negative
+    # removes some of negative instances
+    # positive could mean that the instance has been nominated for OR has won best picture!
+
+    return trainRows, testRows, favoriteCols
+
 
 features = csvToArray("features.csv")
 featNames = features[0]
@@ -38,86 +106,43 @@ for i in range(len(featNames)):
         titleYearIdx = i
 print 'year index = ', titleYearIdx
 
-labelOfInterest = 1 #0 means nominee , 1 means winner (for best picture)
+labelOfInterest = 1
 
-#choosing train/test instances based on title_year
-trainRows = []
-testRows = []
+trainRowsReg, testRowsReg, favColsReg = getTrainTestRowsAndCols(2, titleYearIdx)
+trainRows, testRows, favCols = getTrainTestRowsAndCols(labelOfInterest, titleYearIdx)
 
-nomineeSum = 0
+for i in range(len(features[0])):
+    if (i in favCols) or (i in favColsReg):
+        features[:, i] = preprocessing.scale(features[:, i])
 
-for i in range(len(features)):
-    if labelOfInterest == 0:
-        if float(features[i][titleYearIdx]) > 2010:
-            testRows.append(i)
-        else:
-            trainRows.append(i)
-    elif labelOfInterest == 1:
-        if labels[i][0] == 1:
-            nomineeSum += 1
-            #if float(features[i][titleYearIdx]) > 2010:
-            if int(math.floor(features[i][titleYearIdx])) % 4 == 0:
-                testRows.append(i)
-            else:
-                trainRows.append(i)
+reg = svm.SVR(C=0.005, kernel='poly', degree=3, max_iter=1000000)
 
+predicted = cross_validation.cross_val_predict(reg, (features[trainRowsReg, :])[:, favColsReg], labels[trainRowsReg, 2], cv=10)
+print 'SVR cross_val_predict = ', r2_score(labels[trainRowsReg, 2], predicted)
 
-#prints percentage of train and test
-if labelOfInterest == 0:
-    print len(trainRows) / len(features) , len(testRows) / len(features)
-elif labelOfInterest == 1:
-    print len(trainRows) / nomineeSum, len(testRows) / nomineeSum
+# scores = cross_validation.cross_val_score(reg, (features[trainRowsReg, :])[:, favColsReg], labels[trainRowsReg, 2], cv=10)
+# print scores, 'avg = ', sum(scores)/10
 
+###=================== SVC =======================
 
-#feature selection based on correlation values
-correlation = csvToArray("feature_correlation_results.csv")
+clf = svm.SVC(C=1.5, kernel='poly', degree=3, max_iter=1000000)
 
-favoriteCols = []
-favoriteColsNames = []
+predicted = cross_validation.cross_val_predict(clf, (features[trainRows, :])[:, favCols],
+                                               labels[trainRows, labelOfInterest], cv=10)
 
-for i in range(1, len(correlation)):
-    if correlation[i][0] in featIdxMap:
-        if math.fabs(float(correlation[i][1 + labelOfInterest])) > 0.1:
-            # print correlation[i][0]
-            favoriteCols.append(featIdxMap[correlation[i][0]])
-            favoriteColsNames.append(correlation[i][0])
+print 'SVC cross_val_predict = ', accuracy_score(labels[trainRows, labelOfInterest], predicted)
 
-print 'favoritCols = ', len(favoriteCols)
+sum = 0
+sum_true = 0
+for h in range(len(trainRows)):
+    i = trainRows[h]
+    if labels[i][labelOfInterest] == 0:
+        sum += 1
+        if predicted[h] == 0:
+            sum_true += 1
+        print '%d: \t t=%d p=%d' % (i, labels[i][labelOfInterest], predicted[h])
 
-print favoriteColsNames
+print 'total = %d, percent = %f' % (sum, sum_true/sum)
 
-start_time = time.time()
-
-
-# making test set half positive and half negative
-# removes some of negative instances
-# positive could mean that the instance has been nominated for OR has won best picture!
-
-tmp = []
-for i in range(len(testRows)):
-    if labels[testRows[i]][labelOfInterest] == 1:
-        tmp.append(testRows[i])
-
-positiveNum = len(tmp)
-for i in range(len(testRows)):
-    if labels[testRows[i]][labelOfInterest] == 0 and positiveNum > 0:
-        tmp.append(testRows[i])
-        positiveNum -= 1
-
-testRows = tmp
-
-print 'test length = ', len(testRows)
-
-#fitting SVM Classifier to data
-clf = svm.SVC(kernel='poly', degree=3, max_iter=1000000)
-clf.fit((features[trainRows, :])[:, favoriteCols], labels[trainRows, labelOfInterest])
-y_pred = clf.predict((features[testRows, :])[:, favoriteCols])
-y_test = labels[testRows, labelOfInterest]
-acc = np.mean((y_test-y_pred)==0)
-
-#prints actual label and predicted label to compare
-print '======================'
-for i in range(len(y_pred)):
-    print (testRows[i] + 2), ' = ', y_pred[i], ' ', y_test[i]
-
-print 'accuracy = %f' %(np.mean((y_test-y_pred)==0))
+# scores = cross_validation.cross_val_score(clf, (features[trainRows, :])[:, favCols], labels[trainRows, labelOfInterest], cv=10)
+# print scores, 'avg = ', sum(scores)/10
