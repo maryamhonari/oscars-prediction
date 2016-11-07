027a0366ccafef5058a17d869cf41b5f246a46fa
Omar Elazhary <omazhary@gmail.com>
2016-11-06 16:00:11 -0800

    Updated the correlation script to use new preprocessor functionality.

--- scripts/feature_correlation.py
+++ scripts/feature_correlation.py
@@ -20,32 +20,22 @@ to identify the most influential features.
 
 preprocessor = DataPreprocessor(['Nominated Best Picture',
                                 'Won Best Picture', 'Num of Awards'],
-                                ['genres', 'plot_keywords'],
-                                'final_dataset_no_duplicates.csv')
+                                ['genres', 'plot_keywords', 'movie_imdb_link'],
+                                'movies_original.csv')
 preprocessor.preprocess()
 
-feature_vectors = preprocessor.split_features()
-
-# Need to convert textual attributes to a numeric format.
-
-for index, features in enumerate(feature_vectors):
-    test_index = 0
-    if (features[test_index] == ''):
-        test_index = random.randrange(1, len(features))
-    if (not isinstance(features[test_index], numbers.Number) and
-            not isinstance(features[test_index], decimal.Decimal)):
-        encoder = preprocessing.LabelEncoder()
-        encoder.fit(features)
-        feature_vectors[index] = encoder.transform(features)
+preprocessor.numerify()
 
 # Assuming that the features are normally distributed, we can check the
 # feature-label correlation using the pearson correlation coefficient.
 
+features = map(list, zip(*preprocessor.features_numerical))
+
 print "feature,nominated_best_picture,won_best_picture,num_of_awards"
-for index, feature_vector in enumerate(feature_vectors):
+for index, feature_vector in enumerate(features):
     result = preprocessor.column_headers[index] + ","
     for label_vector in preprocessor.labels:
-        feature_array = np.array(feature_vectors[index]).astype(np.float)
+        feature_array = np.array(feature_vector).astype(np.float)
         label_array = np.array(label_vector).astype(np.float)
         result += str(scipy.stats.pearsonr(feature_array, label_array)[0])
         result += ","
